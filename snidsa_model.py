40b63943d84e1ed3b2d1ec02ed58bebd381edc1e
CodeWithSonny <118931763+CodeWithSonny@users.noreply.github.com>
2023-02-26 12:14:12 +0000

    Bumped patch version.

--- snidsa_model.py
+++ snidsa_model.py
@@ -7,4 +7,5 @@ class SNIDSA(object):
         self.num_nodes = config.num_nodes
         self.embedding_dim = config.embedding_dim
         self.hidden_dim = config.hidden_dim
-        self.num_layers = config.
+        self.num_layers = config.num_layers
+        # self.model = config.mod
