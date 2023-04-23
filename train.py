aea5cba327bf8ee4767b5c9114ee003d6e0664e9
CodeWithSonny <118931763+CodeWithSonny@users.noreply.github.com>
2023-04-23 07:25:47 +0000

    JsRender/Gtk.vala.c JsRender/JsRender.vala.c JsRender/Lang.vala.c JsRender/Node.vala.c JsRender/NodeToGlade.vala.c JsRender/NodeToJs.vala.c JsRender/NodeToVala.vala.c JsRender/Roo.vala.c Project/Gtk.vala.c Project/Project.vala.c Project/Roo.vala.c Palete/Gir.vala.c Palete/Gtk.vala.c Palete/Palete.vala.c Palete/Roo.vala.c Builder4/Application.vala.c Builder4/About.vala.c Builder4/DialogConfirm.vala.c Builder4/DialogNewComponent.vala.c Builder4/StandardErrorDialog.vala.c Builder4/DialogSaveTemplate.vala.c Builder4/DialogSaveModule.vala.c Builder4/DialogTemplateSelect.vala.c Builder4/EditProject.vala.c Builder4/Editor.vala.c Builder4/ProjectSettings.vala.c Builder4/WindowRightPalete.vala.c Builder4/WindowLeftTree.vala.c Builder4/WindowRooView.vala.c Builder4/WindowLeftProps.vala.c Builder4/ClutterFiles.vala.c Builder4/WindowLeftProjects.vala.c Builder4/WindowAddProp.vala.c Builder4/GladeView.vala.c Builder4/MainWindow.vala.c Builder4/Main.vala.c

--- train.py
+++ train.py
@@ -29,3 +29,52 @@ class Config(object):
 class Input(object):
     def __init__(self, config, data):
         self.batch_size = config.batch_size
+        self.num_nodes = config.num_nodes
+        self.inputs, self.targets, self.seq_lenghth = batch_generator(data, self.batch_size)
+        self.batch_num = len(self.inputs)
+        self.cur_batch = 0
+
+    def next_batch(self):
+        x = self.inputs[self.cur_batch]
+        y = self.targets[self.cur_batch]
+        sl = self.seq_lenghth[self.cur_batch]
+        self.cur_batch = (self.cur_batch +1) % self.batch_num
+        batch_size = x.shape[0]
+        num_steps = x.shape[1]
+        return x, y, sl, batch_size, num_steps
+
+def rank_eval(pred, true_labels, sl):
+    mrr = 0
+    ac1 = 0
+    ac5 = 0
+    ac10 = 0
+    ac50 = 0
+    ac100 = 0
+    num_nodes = pred.shape[2]
+    for i in range(len(sl)):
+        length = sl[i]
+        for j in range(length):
+            y_pos = true_labels[i][j]
+            predY = pred[i][j][y_pos]
+            rank = 1.
+            for k in range(num_nodes):
+                if pred[i][j][k]> predY:
+                    rank += 1.
+            if rank <= 1:
+                ac1 += 1./float(length)
+            if rank <= 5:
+                ac5 += 1./float(length)
+            if rank <= 10:
+                ac10 += 1./float(length)
+            if rank <= 50:
+                ac50 += 1./float(length)
+            if rank <= 100:
+                ac100 += 1./float(length)
+            mrr += (1./rank)/float(length)
+    return mrr, ac1, ac5, ac10, ac50, ac100
+
+def args_setting(config):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lr", type=float, help="learning rate")
+    parser.add_argument("-x", "--xdim", type=int, help="embedding dimension")
+    parser.add_argument("-e", "--hdim", type=int, help="hidden dimension")
